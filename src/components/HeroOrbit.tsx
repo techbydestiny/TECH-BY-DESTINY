5c108408d08a8a6f41482230ff406ac08644432a
oduagbondestiny <oduagondestiny@gmail.com>
2025-09-15 15:31:10 +0100

    improving the hero section

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -4,13 +4,15 @@ export const HeroOrbit = ({children, size, rotation }: PropsWithChildren<{ size:
 {
     return(
          <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className=" size-[800px] justify-start" style={{
+        <div className="" style={{
 
           transform: `rotate(${rotation}deg)`,
           height: `${size}px`,
           width:  `${size}px`,
         }}>
-          <div className=" inline-flex">
+          <div className=" inline-flex" style={{
+            transform: `rotate(${rotation * -1}deg)`,
+          }}>
            {children}
           </div>
         </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,17 +6,44 @@ import { TypeAnimation } from 'react-type-animation'
 import heroimage from '@/assets/images/my-hero-image.png'
 import ArrowDown from '@/assets/icons/arrow-down.svg'
 import grainImage from '@/assets/images/grain.jpg'
+import StarIcon from '@/assets/icons/star.svg'
+import SparkleIcon from "@/assets/icons/sparkle.svg"
+
+import { HeroOrbit } from "@/components/HeroOrbit";
 
 export const HeroSection = () => {
 
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
       <div className="absolute inset-0 -z-30 opacity-5" style={{ backgroundImage: `url(${grainImage.src})` }}>
       </div>
-     <div className="w-[120px] h-[120px] md:w-[620px] md:h-[620px] hero-ring"></div>
-      <div className="w-[220px] h-[220px] md:w-[820px] md:h-[820px] hero-ring"></div>
-      <div className="w-[420px] h-[420px] md:w-[1020px] md:h-[1020px] hero-ring"></div>
-      <div className="w-[520px] h-[520px] md:w-[1220px] md:h-[1220px] hero-ring"></div>
+      <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
+      <HeroOrbit size={800} rotation={-72}>
+        <StarIcon className='size-28 text-emerald-300'/>
+      </HeroOrbit> 
+      <HeroOrbit size={550} rotation={20}>
+        <StarIcon className='size-12 text-emerald-300'/>
+      </HeroOrbit> 
+      <HeroOrbit size={590} rotation={98}>
+        <StarIcon className='size-8 text-emerald-300'/>
+      </HeroOrbit> 
+
+      <HeroOrbit size={430} rotation={-14}>
+        <SparkleIcon className='size-8 text-emerald-300/20'/>
+      </HeroOrbit> 
+      <HeroOrbit size={440} rotation={79}>
+        <SparkleIcon className='size-5 text-emerald-300/20'/>
+      </HeroOrbit> 
+      <HeroOrbit size={530} rotation={178}>
+        <SparkleIcon className='size-10 text-emerald-300/20'/>
+      </HeroOrbit> 
+      <HeroOrbit size={710} rotation={144}>
+        <SparkleIcon className='size-14 text-emerald-300/20'/>
+      </HeroOrbit> 
+
       <div className='container'>
         <div className="flex flex-col items-center">
           <Image src={heroimage} alt='heroimg' className="size-[100px]"/>
