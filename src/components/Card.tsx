a576b93b2761277773bdf87819467f4d6c43df39
oduagbondestiny <oduagondestiny@gmail.com>
2025-09-17 18:59:26 +0100

    fixing  framer motions card animation error

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,18 @@
 "use client";
-import { ComponentPropsWithRef } from "react";
+import { ComponentPropsWithRef, ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 import grainImage from "@/assets/images/grain.jpg";
 
-import { motion } from "framer-motion";
+import { motion, HTMLMotionProps } from "framer-motion";
 
-type CardProps = ComponentPropsWithRef<"div"> & {
-  variants?: any;
-  initial?: any;
-  whileInView?: any;
-  transition?: any;
+type CardProps = HTMLMotionProps<"div"> & {
+    children ?: ReactNode;
+    className ?: string;
 };
 
 export const Card = ({
   className,
   children,
-  variants,
-  initial,
-  whileInView,
-  transition,
   ...other
 }: CardProps) => {
   return (
@@ -27,10 +21,6 @@ export const Card = ({
         "bg-gray-800 rounded-3xl relative after:content-[''] z-0 after:z-10 overflow-hidden after:absolute after:inset-0 md:text-4xl after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none",
         className
       )}
-      variants={variants}
-      initial={initial}
-      whileInView={whileInView}
-      transition={transition}
       viewport={{ once: false, amount: 0.3 }}
       {...other}
     >
