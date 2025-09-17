cfa7449ef22de57ade9df57ccdfba3dffd981e86
oduagbondestiny <oduagondestiny@gmail.com>
2025-09-17 20:00:16 +0100

    adding scrowintoview animation to navbar

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,18 @@
 export default function Header() {
+
+     const scrollToSection = (id:string) => {
+        const section = document.getElementById(id);
+        if (section) {
+          section.scrollIntoView({ behavior: "smooth" });
+        }
+    };
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop:blur">
         <a href="/" className="nav-item">Home</a>
-        <a href="#Projects" className="nav-item">Projects</a>
-        <a href="#About" className="nav-item">About</a>
-        <a href="#Contact" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contacts</a>
+        <a onClick={() => scrollToSection('Project')}className="nav-item">Projects</a>
+        <a onClick={() => scrollToSection("About")}className="nav-item">About</a>
+        <a onClick={() => scrollToSection("Contact")} className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contacts</a>
       </nav>
     </div>
   );
